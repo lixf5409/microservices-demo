bb5fbb2ea8ba0d9ca51a6aca3dc934af0511d7c2
chengqp <cwezpeng@163.com>
2019-11-22 10:48:01 +0800

    修改用户表脚本

--- service-user/src/main/etc/demouser.sql
+++ service-user/src/main/etc/demouser.sql
@@ -1,14 +1,11 @@
-create table if not exists org_user
-(
-	user_id int(32) auto_increment
-		primary key,
-	user_name varchar(30) null,
-	user_code varchar(30) null,
-	dept_id int(32) null,
-	sex varchar(1) null comment '性别',
-	birthday varchar(20) null comment '生日',
-	phone varchar(11) null comment '手机号',
-	remarks varchar(200) null comment '备注'
-)
-charset=utf8;
-
+CREATE TABLE `org_user` (
+  `user_id` int(32) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(30) DEFAULT NULL,
+  `user_code` varchar(30) DEFAULT NULL,
+  `dept_id` int(32) DEFAULT NULL,
+  `sex` varchar(1) DEFAULT NULL COMMENT '性别',
+  `birthday` varchar(20) DEFAULT NULL COMMENT '生日',
+  `phone` varchar(11) DEFAULT NULL COMMENT '手机号',
+  `remarks` varchar(200) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`user_id`)
+) ;
