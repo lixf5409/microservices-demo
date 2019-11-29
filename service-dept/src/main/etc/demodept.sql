5e9d4fd0219db83d8b7e5dbe7c45b6f09033a41a
lixf <16077045@qq.com>
2019-11-29 15:49:10 +0800

    dept项目 sharding-jdbc读写分离

--- service-dept/src/main/etc/demodept.sql
+++ service-dept/src/main/etc/demodept.sql
@@ -2,8 +2,6 @@ CREATE TABLE `org_dept` (
   `dept_id` int(32) NOT NULL AUTO_INCREMENT,
   `dept_name` varchar(30) DEFAULT NULL,
   `dept_code` varchar(30) DEFAULT NULL,
+  `parent_id` int(32) NULL,
   PRIMARY KEY (`dept_id`)
 ) ;
-
-ALTER TABLE `org_dept`
-ADD COLUMN `parent_id` int(32) NULL AFTER `dept_code`;
